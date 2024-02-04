610c0e64975b4d9b9d492546d017492f774883d6
Ali Sadeghi <ali909392@gmail.com>
2024-02-04 16:31:03 +0330

    End of UI / Start work on Functionality

--- TS/index.ts
+++ TS/index.ts
@@ -1,1 +1,15 @@
 console.log("hi");
+
+// function checkFirstCharDirection() {
+//     var inputElement = document.getElementById("myInput");
+//     var inputValue = inputElement.value.trim(); // Remove leading/trailing whitespaces
+
+//     // Check if the first character is an English letter
+//     if (/^[a-zA-Z]/.test(inputValue)) {
+//         inputElement.dir = "ltr";
+//     }
+//     // If the first character is not an English letter, assume it's Persian
+//     else {
+//         inputElement.dir = "rtl";
+//     }
+// }
