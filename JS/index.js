af39a267c668a5c2a3129da1324f3828bf9bbc40
Ali Sadeghi <ali909392@gmail.com>
2024-02-05 01:19:28 +0330

    Add core functionality of app

--- JS/index.js
+++ JS/index.js
@@ -1,3 +1,98 @@
 "use strict";
-console.log("hi");
+const setDirs = document.querySelectorAll(".set-dir");
+const submitBtn = document.querySelector(".arrow-btn");
+const orderList = document.querySelector(".order-list");
+const inputNames = document.querySelectorAll(".input-name");
+const outputNames = document.querySelectorAll(".output-name");
+let ListOfNames = [];
+inputNames[0].value = "1";
+inputNames[1].value = "2";
+inputNames[2].value = "3";
+inputNames[3].value = "4";
+const changeDir = (HTMLValue) => {
+    if (/^[a-zA-Z]/.test(HTMLValue.value)) {
+        HTMLValue.style.direction = "ltr";
+        console.log("ENG");
+    }
+    else {
+        HTMLValue.style.direction = "rtl";
+        console.log("PER");
+    }
+};
+setDirs.forEach((setDir) => {
+    setDir.addEventListener("input", () => {
+        const inputValue = setDir;
+        changeDir(inputValue);
+    });
+});
+const shuffleArray = (array) => {
+    const elementsArray = Array.from(array);
+    console.log(array);
+    console.log(elementsArray);
+    for (let i = elementsArray.length - 1; i > 0; i--) {
+        const j = Math.floor(Math.random() * (i + 1));
+        [elementsArray[i], elementsArray[j]] = [
+            elementsArray[j],
+            elementsArray[i],
+        ];
+    }
+    console.log(elementsArray);
+    orderList.innerHTML = "";
+    for (let i = 0; i < 4; i++) {
+        console.log(elementsArray[i]);
+        const liElement = document.createElement("li");
+        liElement.classList.add(`li-n${i}`);
+        liElement.appendChild(elementsArray[i]);
+        orderList.appendChild(liElement);
+    }
+};
+const changePosition = (nameCounter) => {
+    const allLis = orderList.querySelectorAll("li");
+    console.log(allLis);
+    const randomNumber = Math.floor(Math.random() * (nameCounter - 1));
+    orderList.appendChild(allLis[randomNumber]);
+};
+const elements = document.querySelectorAll(".my-elements");
+submitBtn?.addEventListener("click", () => {
+    inputNames.forEach((inputName) => {
+        const nameValue = inputName;
+        console.log(nameValue.value);
+    });
+    console.log("---------");
+    ListOfNames = [];
+    let nameCounter = 0;
+    for (let i = 0; i < 4; i++) {
+        const inputName = inputNames[i];
+        if (inputName.value) {
+            nameCounter++;
+            console.log(inputName.value);
+            ListOfNames.push(inputName.value);
+        }
+    }
+    for (let i = 0; i < ListOfNames.length; i++) {
+        const outputName = outputNames[i];
+        outputName.value = ListOfNames[i];
+        outputName.style.opacity = "1";
+        changeDir(outputName);
+    }
+    for (let i = ListOfNames.length; i < 4; i++) {
+        const outputName = outputNames[i];
+        outputName.value = "";
+        outputName.style.opacity = "0.5";
+    }
+    const allLis = orderList.querySelectorAll("li");
+    for (let i = nameCounter; i < allLis.length; i++) {
+        allLis[i].remove();
+    }
+    const numberOfIterations = nameCounter * 5 + Math.floor(Math.random() * 2);
+    for (let i = 0; i < numberOfIterations; i++) {
+        setTimeout(() => {
+            changePosition(nameCounter);
+        }, i * 300);
+    }
+});
+const glHf = document.querySelector(".gl-hf");
+glHf.addEventListener("click", () => {
+    glHf.textContent = "Good Luck, Have Fun :)";
+});
 //# sourceMappingURL=index.js.map
--- TS/index.ts
+++ TS/index.ts
@@ -1,15 +1,151 @@
-console.log("hi");
+const setDirs = document.querySelectorAll(".set-dir") as NodeListOf<Element>;
+const submitBtn = document.querySelector(".arrow-btn") as HTMLElement;
+const orderList = document.querySelector(".order-list") as HTMLElement;
+const inputNames = document.querySelectorAll(
+    ".input-name"
+) as NodeListOf<Element>;
+const outputNames = document.querySelectorAll(
+    ".output-name"
+) as NodeListOf<Element>;
 
-// function checkFirstCharDirection() {
-//     var inputElement = document.getElementById("myInput");
-//     var inputValue = inputElement.value.trim(); // Remove leading/trailing whitespaces
+let ListOfNames: string[] = [];
 
-//     // Check if the first character is an English letter
-//     if (/^[a-zA-Z]/.test(inputValue)) {
-//         inputElement.dir = "ltr";
+(inputNames[0] as HTMLInputElement).value = "1";
+(inputNames[1] as HTMLInputElement).value = "2";
+(inputNames[2] as HTMLInputElement).value = "3";
+(inputNames[3] as HTMLInputElement).value = "4";
+
+const changeDir = (HTMLValue: HTMLInputElement) => {
+    if (/^[a-zA-Z]/.test(HTMLValue.value)) {
+        HTMLValue.style.direction = "ltr";
+        console.log("ENG");
+    } else {
+        HTMLValue.style.direction = "rtl";
+        console.log("PER");
+    }
+};
+
+setDirs.forEach((setDir) => {
+    setDir.addEventListener("input", () => {
+        const inputValue = setDir as HTMLInputElement;
+
+        changeDir(inputValue);
+    });
+});
+
+// console.log(setDirs);
+
+// const shuffleArray = (array: NodeListOf<Element>) => {
+//     for (let i = array.length - 1; i > 0; i--) {
+//         const j = Math.floor(Math.random() * (i + 1));
+//         [array[i], array[j]] = [array[j], array[i]];
 //     }
-//     // If the first character is not an English letter, assume it's Persian
-//     else {
-//         inputElement.dir = "rtl";
-//     }
-// }
+// };
+
+const shuffleArray = (array: NodeListOf<Element>) => {
+    // Convert NodeList to an array
+    const elementsArray: Element[] = Array.from(array);
+
+    console.log(array);
+    console.log(elementsArray);
+
+    for (let i = elementsArray.length - 1; i > 0; i--) {
+        const j = Math.floor(Math.random() * (i + 1));
+        [elementsArray[i], elementsArray[j]] = [
+            elementsArray[j],
+            elementsArray[i],
+        ];
+    }
+
+    console.log(elementsArray);
+
+    orderList.innerHTML = "";
+
+    for (let i = 0; i < 4; i++) {
+        console.log(elementsArray[i]);
+        const liElement = document.createElement("li");
+        liElement.classList.add(`li-n${i}`);
+        liElement.appendChild(elementsArray[i]);
+        orderList.appendChild(liElement);
+    }
+};
+
+const changePosition = (nameCounter: number) => {
+    const allLis = orderList.querySelectorAll("li");
+    console.log(allLis);
+
+    const randomNumber = Math.floor(Math.random() * (nameCounter - 1));
+
+    orderList.appendChild(allLis[randomNumber]);
+};
+
+const elements: NodeListOf<Element> = document.querySelectorAll(".my-elements");
+// shuffleArray(elements);
+
+submitBtn?.addEventListener("click", () => {
+    inputNames.forEach((inputName) => {
+        const nameValue = inputName as HTMLInputElement;
+        console.log(nameValue.value);
+    });
+
+    console.log("---------");
+    ListOfNames = [];
+
+    let nameCounter = 0;
+
+    for (let i = 0; i < 4; i++) {
+        const inputName = inputNames[i] as HTMLInputElement;
+        if (inputName.value) {
+            nameCounter++;
+            console.log(inputName.value);
+            ListOfNames.push(inputName.value);
+        }
+    }
+
+    for (let i = 0; i < ListOfNames.length; i++) {
+        const outputName = outputNames[i] as HTMLInputElement;
+        outputName.value = ListOfNames[i];
+        outputName.style.opacity = "1";
+        changeDir(outputName);
+    }
+
+    for (let i = ListOfNames.length; i < 4; i++) {
+        const outputName = outputNames[i] as HTMLInputElement;
+
+        outputName.value = "";
+        outputName.style.opacity = "0.5";
+    }
+
+    // shuffleArray(outputNames);
+
+    const allLis = orderList.querySelectorAll("li");
+
+    for (let i = nameCounter; i < allLis.length; i++) {
+        allLis[i].remove();
+    }
+
+    const numberOfIterations = nameCounter * 5 + Math.floor(Math.random() * 2);
+
+    for (let i = 0; i < numberOfIterations; i++) {
+        setTimeout(() => {
+            changePosition(nameCounter);
+        }, i * 300);
+    }
+
+    // const allLis = orderList.querySelectorAll("li");
+
+    // for (let i = 0; i < 4; i++) {
+    //     if (
+    //         (allLis[i].querySelector("input") as HTMLElement).style.opacity ===
+    //         "0.5"
+    //     ) {
+    //         orderList.appendChild(allLis[i]);
+    //         console.log("LLL", i);
+    //     }
+    // }
+});
+
+const glHf = document.querySelector(".gl-hf") as HTMLElement;
+glHf.addEventListener("click", () => {
+    glHf.textContent = "Good Luck, Have Fun :)";
+});
